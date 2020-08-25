1c41612d2df6342c3473dc7a5fec1e5fe87fcd04
Cameron Whyte <public@cameronwhyte.me>
2020-08-25 16:53:44 +0100

    ported!

--- src/main/java/me/cameronwhyte/mods/lavasmelting/LavaSmelting.java
+++ src/main/java/me/cameronwhyte/mods/lavasmelting/LavaSmelting.java
@@ -1,28 +1,11 @@
 package me.cameronwhyte.mods.lavasmelting;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.event.server.ServerTickCallback;
-import net.minecraft.item.ItemStack;
-import net.minecraft.server.network.ServerPlayerEntity;
-
-import java.util.List;
 
 public class LavaSmelting implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        ServerTickCallback.EVENT.register(minecraftServer -> {
-            List<ServerPlayerEntity> playerList = minecraftServer.getPlayerManager().getPlayerList();
-            playerList.forEach(player -> {
-                for(int i=0; i < player.inventory.size();) {
-                    ItemStack item = player.inventory.getStack(i);
-                    if(item.getOrCreateTag().contains("type")) {
-                        item.removeSubTag("type");
-                    }
-                    i+=1;
-                }
-            });
-        });
         System.out.println("Lava Smelting has been loaded!");
     }
 }
--- src/main/java/me/cameronwhyte/mods/lavasmelting/mixins/NoItemSmelt.java
+++ src/main/java/me/cameronwhyte/mods/lavasmelting/mixins/NoItemSmelt.java
@@ -3,7 +3,7 @@ package me.cameronwhyte.mods.lavasmelting.mixins;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ItemEntity;
-import net.minecraft.inventory.BasicInventory;
+import net.minecraft.inventory.SimpleInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.recipe.RecipeType;
 import net.minecraft.recipe.SmeltingRecipe;
@@ -11,48 +11,36 @@ import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
-import java.util.Objects;
 import java.util.Optional;
 import java.util.Random;
 
 @Mixin(ItemEntity.class)
 public abstract class NoItemSmelt extends Entity {
 
-
     @Shadow public abstract ItemStack getStack();
 
-    private boolean debounce = false;
-    private Random random = new Random();
+    @Shadow public abstract void setStack(ItemStack stack);
+
+    private final Random random = new Random();
 
     public NoItemSmelt(EntityType<?> type, World world) {
         super(type, world);
     }
 
     public void baseTick() {
-        if(!debounce) {
-            if(!this.getStack().getOrCreateTag().contains("type")) {
-                if(isInLava() || isOnFire()) {
-                    debounce = true;
-                    Optional<SmeltingRecipe> maysmelt = this.world.getRecipeManager().getFirstMatch(RecipeType.SMELTING, new BasicInventory(this.getStack()), this.world);
-                    if(maysmelt.isPresent()) {
-                        ItemEntity smelted = new ItemEntity(world, this.getPos().x, this.getPos().y, this.getPos().z, maysmelt.get().getOutput());
-                        smelted.getStack().setCount(this.getStack().getCount());
-                        smelted.getStack().getOrCreateTag().putString("type", "lavasmelting");
-                        this.world.spawnEntity(smelted);
-                    }
-                }
-            }
-            if(this.getStack().getOrCreateTag().contains("type")) {
-                if(Objects.requireNonNull(this.getStack().getOrCreateTag().get("type")).asString().equals("lavasmelting")) {
-                    if(isInLava() || isOnFire()) {
-                        this.extinguish();
-                        this.setInvulnerable(true);
-                        this.setVelocity(random.nextGaussian() / 8, .15, random.nextGaussian() / 8);
-                    }
-                }
-            }
+        if(this.isInvulnerable() && this.isInLava()) {
+            this.setVelocity(this.random.nextGaussian() / 8, .15, this.random.nextGaussian() / 8);
         }
+        Optional<SmeltingRecipe> canBeSmelted = this.world.getRecipeManager().getFirstMatch(RecipeType.SMELTING, new SimpleInventory(this.getStack()), this.world);
+        canBeSmelted.ifPresent(furnaceRecipe -> {
+            if(this.isInLava() || this.isOnFire()) {
+                this.extinguish();
+                this.setInvulnerable(true);
+                ItemStack output = furnaceRecipe.getOutput();
+                output.setCount(this.getStack().getCount());
+                this.setStack(output);
+            }
+        });
         super.baseTick();
     }
-
 }
